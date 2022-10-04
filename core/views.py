d560949500d835b15e5e63156c6ccc2a69dc7b19
Saheel Sakharkar <saheel.sakharkar@gmail.com>
2022-10-04 10:41:06 -0500

    fix: removed html references to 'custom_date'

--- core/views.py
+++ core/views.py
@@ -261,13 +261,15 @@ def delete_tag(tag_id):
 
 
 # Custom Filter
-"""@current_app.template_filter()
+"""
+@current_app.template_filter()
 def custom_date(date):
     '''
         Convert a datetime into custom format like: Sep 12,2017 19:07:32
     '''
     date = datetime.datetime.strptime(date, '%Y-%m-%d %H:%M:%S')
-    return date.strftime('%b %d,%Y %H:%M:%S')"""
+    return date.strftime('%b %d,%Y %H:%M:%S')
+"""
 
 
 @core.route("/profile/settings/")
