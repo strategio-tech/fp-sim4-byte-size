e2dba40b16abb3e68fb2df395318e6f8d87d5c63
Saheel Sakharkar <saheel.sakharkar@gmail.com>
2022-10-05 16:51:59 -0500

    fix: absolute filepath try again

--- terraform/main.tf
+++ terraform/main.tf
@@ -47,7 +47,7 @@ resource "aws_instance" "web" {
   instance_type          = "t2.micro"
   vpc_security_group_ids = [aws_security_group.web-sg.id]
 
-  user_data = file("${path.module}/scripts/init.sh")
+  user_data = file("${path.cwd}/../scripts/init.sh")
 }
 
 resource "aws_security_group" "web-sg" {
