190fa32f26fc124197273afab2ec44e69a2460f1
Saheel Sakharkar <saheel.sakharkar@gmail.com>
2022-10-05 16:56:11 -0500

    fix: user_data now uses raw bash script

--- terraform/main.tf
+++ terraform/main.tf
@@ -47,7 +47,20 @@ resource "aws_instance" "web" {
   instance_type          = "t2.micro"
   vpc_security_group_ids = [aws_security_group.web-sg.id]
 
-  user_data = file("${path.cwd}/../scripts/init.sh")
+  user_data =   <<-EOF
+                #!/bin/bash
+                apt-get update -y
+                apt-get -y install python3-pip
+                cd ~
+                git clone https://github.com/strategio-tech/fp-sim4-byte-size.git
+                cd fp-sim4-byte-size
+                apt install software-properties-common
+                add-apt-repository -y ppa:deadsnakes/ppa
+                apt update -y
+                apt install python3.8 -y
+                pip3 install -t /usr/local/lib/python3.8/dist-packages -r requirements.txt
+                python3.8 manage.py
+                EOF
 }
 
 resource "aws_security_group" "web-sg" {
